3f51a7292079083deac10de4d4cc5dd88df9df50
andichaerul <andichaerul85@gmail.com>
2018-08-19 17:58:56 +0800

    Mencari start dan tujuan lebih cepat

--- application/views/found_point.php
+++ application/views/found_point.php
@@ -2,25 +2,35 @@
  foreach ($found_point as $row) {
  	$PointName[] = $row->PointName;
  	$Latitude[] = $row->Latitude;
- 	$Longitude[] = $row->Longitude;
+ 	$Longitude[] = $row->Longitude; 
  }
 ?>
 <?php
-
-for ($x = 0; $x <= 10; $x++) {
-    echo "The number is: $x <br>";
-}  
+for ($x = 0; $x < count($PointName); $x++) {
+	$koma = ",";
+    $carikordinat[] = $Latitude[$x].$koma.$Longitude[$x];
+} 
+$ListCordinate = join("|",$carikordinat);
 ?>
 <?php
-$url = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial%20&origins=-5.096227,20119.510818%20&destinations=-5.091716,20119.505282|-5.089782,20119.505336|-5.089953,%20119.517063%20&key=AIzaSyBHW1caUelglRxZTENPSzbdJaupH9MntFs';
+$url = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial%20&origins=-5.096227,%20119.510818%20&destinations='.$ListCordinate.'&key=AIzaSyBHW1caUelglRxZTENPSzbdJaupH9MntFs';
 $JSON = file_get_contents($url);
 
 // echo the JSON (you can echo this to JavaScript to use it there)
-echo $JSON;
+//echo $JSON;
 
 // You can decode it to process it in PHP
-$data = json_decode($JSON);
-var_dump($data);
+$data = json_decode($JSON,true);
+echo "<pre>";
+print_r($data);
+echo "</pre>";
+for ($x = 0; $x < count($PointName); $x++) {
+    $value[] = $data['rows']['0']['elements'][$x]['distance']['value'];
+}
+$cari = min($value);   
+echo $cari;
+$carikordinat = array_search($cari,$value);
+echo "".$PointName[$carikordinat]."".$Latitude[$carikordinat].','.$Longitude[$carikordinat]."";
 ?>
 <?php
 //for ($x = 0; $x < count($PointName); $x++) {

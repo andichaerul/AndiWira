e2ae873b96ade083b0eae2c0bcf541b89adf63d7
andichaerul <andichaerul85@gmail.com>
2018-08-23 11:37:06 +0800

    Revert "Errorr Function"
    This reverts commit 9b513341a1eccba9ffd88fcc128f056558ea108c.

--- application/views/found_point.php
+++ application/views/found_point.php
@@ -25,43 +25,10 @@ for ($x = 0; $x < count($data['rows']['0']['elements']); $x++) {
     $HitungJarak[$x] = $data['rows']['0']['elements'][$x]['distance']['value'];
 }
 asort($HitungJarak,true);
-//print_r(array_keys($HitungJarak));
+print_r(array_keys($HitungJarak));
 $KeyDistanceMin = (array_keys($HitungJarak));
 print_r($PointName[$KeyDistanceMin['0']]);
-//echo"<pre>";
-//print_r($data['rows']['0']['elements']);
-//echo"</pre>";
+echo"<pre>";
+print_r($data['rows']['0']['elements']);
+echo"</pre>";
 ?> 
-<?php
-//pembuatan fungsi
-function perkalian($DariDanTujuan)
-{
- foreach ($found_point as $row) {
- 	$PointName[] = $row->PointName;
- 	$Latitude[] = $row->Latitude;
- 	$Longitude[] = $row->Longitude;
- }	
-for ($x = 0; $x < count($PointName); $x++) {
-	$koma = ',';
-    $ListCordinate[$x] = $Latitude[$x].$koma.$Longitude[$x];
-
-}
-$RenderListCordinate = join("|",$ListCordinate);	
-$url = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins='.$DariDanTujuan.'&destinations='.$RenderListCordinate.'&key=AIzaSyBHW1caUelglRxZTENPSzbdJaupH9MntFs';
-$JSON = file_get_contents($url);
-$data = json_decode($JSON,true);
-for ($x = 0; $x < count($data['rows']['0']['elements']); $x++) {
-    $HitungJarak[$x] = $data['rows']['0']['elements'][$x]['distance']['value'];
-}
-asort($HitungJarak,true);
-//print_r(array_keys($HitungJarak));
-$KeyDistanceMin = (array_keys($HitungJarak));
-$hasil =($PointName[$KeyDistanceMin['0']]);
-return $hasil;
-}
-//pemanggilan fungsi
-$hasil=perkalian(4,5);
-echo "Perkalian 4 x 5 adalah $hasil";
-echo "<br />";
-echo "Perkalian 7 x 2 adalah ".perkalian($_GET['dari']);
-?>

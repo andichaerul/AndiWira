fca978d7405ba7de9c525c577c382fbc55de242b
andichaerul <andichaerul85@gmail.com>
2018-08-26 01:17:29 +0800

    perkawinan

--- application/views/found_point.php
+++ application/views/found_point.php
@@ -1,103 +1,30 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <meta charset="utf-8">
-    <title>Directions Service</title>
-    <style>
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-      #map {
-        height: 100%;
-      }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-      #floating-panel {
-        position: absolute;
-        top: 10px;
-        left: 25%;
-        z-index: 5;
-        background-color: #fff;
-        padding: 5px;
-        border: 1px solid #999;
-        text-align: center;
-        font-family: 'Roboto','sans-serif';
-        line-height: 30px;
-        padding-left: 10px;
-      }
-    </style>
-  </head>
-  <body>
-    <div id="floating-panel">
-    <b>Start: </b>
-    <select id="start">
-      <option value="chicago, il">Chicago</option>
-      <option value="st louis, mo">St Louis</option>
-      <option value="joplin, mo">Joplin, MO</option>
-      <option value="oklahoma city, ok">Oklahoma City</option>
-      <option value="amarillo, tx">Amarillo</option>
-      <option value="gallup, nm">Gallup, NM</option>
-      <option value="flagstaff, az">Flagstaff, AZ</option>
-      <option value="winona, az">Winona</option>
-      <option value="kingman, az">Kingman</option>
-      <option value="barstow, ca">Barstow</option>
-      <option value="san bernardino, ca">San Bernardino</option>
-      <option value="los angeles, ca">Los Angeles</option>
-    </select>
-    <b>End: </b>
-    <select id="end">
-      <option value="chicago, il">Chicago</option>
-      <option value="st louis, mo">St Louis</option>
-      <option value="joplin, mo">Joplin, MO</option>
-      <option value="oklahoma city, ok">Oklahoma City</option>
-      <option value="amarillo, tx">Amarillo</option>
-      <option value="gallup, nm">Gallup, NM</option>
-      <option value="flagstaff, az">Flagstaff, AZ</option>
-      <option value="winona, az">Winona</option>
-      <option value="kingman, az">Kingman</option>
-      <option value="barstow, ca">Barstow</option>
-      <option value="san bernardino, ca">San Bernardino</option>
-      <option value="los angeles, ca">Los Angeles</option>
-    </select>
-    </div>
-    <div id="map"></div>
-    <script>
-      function initMap() {
-        var directionsService = new google.maps.DirectionsService;
-        var directionsDisplay = new google.maps.DirectionsRenderer;
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 7,
-          center: {lat: 41.85, lng: -87.65}
-        });
-        directionsDisplay.setMap(map);
+<?php
+$my_array = array("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20");
 
-        var onChangeHandler = function() {
-          calculateAndDisplayRoute(directionsService, directionsDisplay);
-        };
-        document.getElementById('start').addEventListener('change', onChangeHandler);
-        document.getElementById('end').addEventListener('change', onChangeHandler);
-      }
+for ($x = 0; $x < $_GET['max_pop']; $x++) {
+    shuffle($my_array);
+    $data[] = array_slice($my_array, 0, rand(3,count($my_array)));
+} 
+echo "<pre>";
+print_r($data);
+echo "</pre>";
+?>
 
-      function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-        directionsService.route({
-          origin: document.getElementById('start').value,
-          destination: document.getElementById('end').value,
-          travelMode: 'DRIVING'
-        }, function(response, status) {
-          if (status === 'OK') {
-            directionsDisplay.setDirections(response);
-          } else {
-            window.alert('Directions request failed due to ' + status);
-          }
-        });
-      }
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCWzBMH3dONJGBKSaEXWZH65ef055fZrHI&callback=initMap">
-    </script>
-  </body>
-</html>
+
+<?php
+if ($data['1']['0'] <> $data['2']['0']) {
+    // mencari potongan data pertama
+    $findcut = array_search($data['2']['0'+'1'],$data['1']);
+    echo array_search($data['2']['0'+'1'],$data['1']);
+    //print_r(array_slice($data['1'], 0 , $findcut)); 
+    $child01 = array_slice($data['1'], 0 , $findcut);
+    //print_r(array_slice($data['2'], 1 , count($data['2'])));
+    $child02 = array_slice($data['2'], 1 , count($data['2']));
+    $stillsame = array_merge($child01,$child02);
+    // Anak Pertama
+    echo "<pre>";
+    print_r(array_unique($stillsame));
+    echo "</pre>";
+    echo "Have a good day!";
+}
+?>

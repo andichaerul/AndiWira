f1fe7e483582e53d8c818aa4bb088e2d9d58305a
andichaerul <andichaerul85@gmail.com>
2018-08-25 23:55:29 +0800

    Revisi Semua karena Google maps api terlalu mahal

--- application/views/found_point.php
+++ application/views/found_point.php
@@ -1,103 +1,103 @@
-<?php
- foreach ($found_point as $row) {
- 	$PointName[] = $row->PointName;
- 	$Latitude[] = $row->Latitude;
- 	$Longitude[] = $row->Longitude;
- }
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="utf-8">
+    <title>Directions Service</title>
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 100%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+      #floating-panel {
+        position: absolute;
+        top: 10px;
+        left: 25%;
+        z-index: 5;
+        background-color: #fff;
+        padding: 5px;
+        border: 1px solid #999;
+        text-align: center;
+        font-family: 'Roboto','sans-serif';
+        line-height: 30px;
+        padding-left: 10px;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="floating-panel">
+    <b>Start: </b>
+    <select id="start">
+      <option value="chicago, il">Chicago</option>
+      <option value="st louis, mo">St Louis</option>
+      <option value="joplin, mo">Joplin, MO</option>
+      <option value="oklahoma city, ok">Oklahoma City</option>
+      <option value="amarillo, tx">Amarillo</option>
+      <option value="gallup, nm">Gallup, NM</option>
+      <option value="flagstaff, az">Flagstaff, AZ</option>
+      <option value="winona, az">Winona</option>
+      <option value="kingman, az">Kingman</option>
+      <option value="barstow, ca">Barstow</option>
+      <option value="san bernardino, ca">San Bernardino</option>
+      <option value="los angeles, ca">Los Angeles</option>
+    </select>
+    <b>End: </b>
+    <select id="end">
+      <option value="chicago, il">Chicago</option>
+      <option value="st louis, mo">St Louis</option>
+      <option value="joplin, mo">Joplin, MO</option>
+      <option value="oklahoma city, ok">Oklahoma City</option>
+      <option value="amarillo, tx">Amarillo</option>
+      <option value="gallup, nm">Gallup, NM</option>
+      <option value="flagstaff, az">Flagstaff, AZ</option>
+      <option value="winona, az">Winona</option>
+      <option value="kingman, az">Kingman</option>
+      <option value="barstow, ca">Barstow</option>
+      <option value="san bernardino, ca">San Bernardino</option>
+      <option value="los angeles, ca">Los Angeles</option>
+    </select>
+    </div>
+    <div id="map"></div>
+    <script>
+      function initMap() {
+        var directionsService = new google.maps.DirectionsService;
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 7,
+          center: {lat: 41.85, lng: -87.65}
+        });
+        directionsDisplay.setMap(map);
 
-<?php
-for ($x = 0; $x < count($PointName); $x++) {
-	$koma = ',';
-    $ListCordinate[$x] = $Latitude[$x].$koma.$Longitude[$x];
+        var onChangeHandler = function() {
+          calculateAndDisplayRoute(directionsService, directionsDisplay);
+        };
+        document.getElementById('start').addEventListener('change', onChangeHandler);
+        document.getElementById('end').addEventListener('change', onChangeHandler);
+      }
 
-}
-$RenderListCordinate = join("|",$ListCordinate);
-?>
-
-<?php
-$url = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins='.$_GET['dari'].'&destinations='.$RenderListCordinate.'&key=AIzaSyBHW1caUelglRxZTENPSzbdJaupH9MntFs';
-$JSON = file_get_contents($url);
-// echo the JSON (you can echo this to JavaScript to use it there)
-//echo $JSON;
-// You can decode it to process it in PHP
-$data = json_decode($JSON,true);
-for ($x = 0; $x < count($data['rows']['0']['elements']); $x++) {
-    $HitungJarak[$x] = $data['rows']['0']['elements'][$x]['distance']['value'];
-}
-asort($HitungJarak,true);
-//print_r(array_keys($HitungJarak));
-$KeyDistanceMin = (array_keys($HitungJarak));
-echo"<pre>";
-$start = $PointName[$KeyDistanceMin['0']];
-print_r($PointName[$KeyDistanceMin['0']]);
-echo"</pre>";
-echo"<pre>";
-//print_r($data['rows']['0']['elements']);
-echo"</pre>";
-?>
-
-
-<?php
-$url1 = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins='.$_GET['tujuan'].'&destinations='.$RenderListCordinate.'&key=AIzaSyBHW1caUelglRxZTENPSzbdJaupH9MntFs';
-$JSON1 = file_get_contents($url1);
-// echo the JSON (you can echo this to JavaScript to use it there)
-//echo $JSON;
-// You can decode it to process it in PHP
-$data1 = json_decode($JSON1,true);
-for ($x = 0; $x < count($data1['rows']['0']['elements']); $x++) {
-    $HitungJarak1[$x] = $data1['rows']['0']['elements'][$x]['distance']['value'];
-}
-asort($HitungJarak1,true);
-//print_r(array_keys($HitungJarak));
-$KeyDistanceMin1 = (array_keys($HitungJarak1));
-
-
-
-echo"<pre>";
-print_r($PointName[$KeyDistanceMin1['0']]);
-echo"</pre>";
-?>
-<?php 
-//echo"<pre>";
-//	print_r($PointName);
-//echo"</pre>";
-
-
-for ($x = 0; $x < $_GET['max_populasi']; $x++) {
-	$keys = array_keys($PointName);
-    shuffle($keys);
-	$populasi[$x] = array_slice($keys, rand(0,3),rand(5,10));
-    
-}
-echo"<pre>";
-print_r($populasi);
-echo"</pre>";
-?>
-<?php
-
-for ($x = 0; $x < count($populasi['1']); $x++) {
-	
-    $waypoint[] = $Latitude[$populasi['1'][$x]].$koma.$Longitude[$populasi['1'][$x]];
-    
-}
-$start = $Latitude[$KeyDistanceMin['0']].$koma.$Longitude[$KeyDistanceMin['0']];
-$finish = $Latitude[$KeyDistanceMin1['0']].$koma.$Longitude[$KeyDistanceMin1['0']];
-$waypointJoin = join("|",$waypoint);
-$url3 = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins='.$start.'|'.$waypointJoin.'|'.$finish.'&destinations='.$start.'|'.$waypointJoin.'|'.$finish.'&key=AIzaSyBHW1caUelglRxZTENPSzbdJaupH9MntFs';
-$JSON3 = file_get_contents($url3);
-
-// echo the JSON (you can echo this to JavaScript to use it there)
-//echo $JSON;
-
-// You can decode it to process it in PHP
-$data3 = json_decode($JSON3, true);
-echo "<pre>";
-print_r($data3);
-
-echo "</pre>";
-for ($x = 0; $x < count($data3['rows']['0']['elements']) ; $x++) {
-    $datadistance[$x] = $data3['rows'][$x]['elements'][$x+'1']['distance']['value'];
-}
-print_r($datadistance);
-?>
+      function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        directionsService.route({
+          origin: document.getElementById('start').value,
+          destination: document.getElementById('end').value,
+          travelMode: 'DRIVING'
+        }, function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCWzBMH3dONJGBKSaEXWZH65ef055fZrHI&callback=initMap">
+    </script>
+  </body>
+</html>
--- application/views/index.php
+++ application/views/index.php
@@ -1,69 +1,28 @@
-<!DOCTYPE html>
-<html>
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
-    <title>Custom Markers</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-    <meta charset="utf-8">
-    <style>
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-      #map {
-        height: 100%;
-      }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-    </style>
+    <title>JQVMap - Indonesia Map</title>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+    <link href="dist/jqvmap.css" media="screen" rel="stylesheet" type="text/css"/>
   </head>
   <body>
-    <div id="map"></div>
-    <?php echo "
-    <script>
-      var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 13,
-          center: new google.maps.LatLng(".$_GET['dari']."),
-          mapTypeId: 'roadmap'
-        });
-
-        var iconBase = '".base_url('img/')."';
-        var icons = {
-          dari: {
-            icon: iconBase + 'dari.png'
-          },
-          tujuan: {
-            icon: iconBase + 'tujuan.png'
-          }
-        };
-
-        var features = [
-          {
-            position: new google.maps.LatLng(".$_GET['dari']."),
-            type: 'dari'
-          }, 
-          {
-            position: new google.maps.LatLng(".$_GET['tujuan']."),
-            type: 'tujuan'
-          }, 
-        ];
-
-        // Create markers.
-        features.forEach(function(feature) {
-          var marker = new google.maps.Marker({
-            position: feature.position,
-            icon: icons[feature.type].icon,
-            map: map
-          });
-        });
-      }
-    </script>"
-    ?>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBHW1caUelglRxZTENPSzbdJaupH9MntFs&callback=initMap">
-    </script>
+    <div id="vmap" style="width: 900px; height: 400px;"></div>
   </body>
+  <script type="text/javascript" src="dist/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="dist/jquery.vmap.js"></script>
+    <script type="text/javascript" src="dist/maps/jquery.vmap.indonesia.js" charset="utf-8"></script>
+    <script>
+      jQuery(document).ready(function () {
+        jQuery('#vmap').vectorMap({
+          map: 'indonesia_id',
+          enableZoom: true,
+          showTooltip: true,
+          selectedColor: null,
+          onRegionClick: function(event, code, region){
+            event.preventDefault();
+          }
+        });
+      });
+    </script>
 </html>

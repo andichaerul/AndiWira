2380470b6ad8f88b8fc9d4e54b5f6c9274fa5eef
andichaerul <andichaerul85@gmail.com>
2018-08-26 02:17:42 +0800

    Berhasil membuat Function

--- application/views/found_point.php
+++ application/views/found_point.php
@@ -12,7 +12,7 @@ echo "</pre>";
 
 
 <?php
-if ($data['1']['0'] <> $data['2']['0']) {
+  if ($data['1']['0'] <> $data['2']['0']) {
     // mencari potongan data pertama
     $findcut = array_search($data['2']['0'+'1'],$data['1']);
     echo array_search($data['2']['0'+'1'],$data['1']);
@@ -25,6 +25,46 @@ if ($data['1']['0'] <> $data['2']['0']) {
     echo "<pre>";
     print_r(array_unique($stillsame));
     echo "</pre>";
-    echo "Have a good day!";
+    $findcut1 = array_search($data['1'][$findcut],$data['2']);
+    $child11 = array_slice($data['2'], 0 , $findcut1);
+    $child12 = array_slice($data['1'], $findcut+1, count($data['1']));
+    $stillsame1 = array_merge($child11,$child12);
+    echo "<pre>";
+    print_r(array_unique($stillsame1));
+    echo "</pre>";
+
 }
+?>
+<?php
+//pembuatan fungsi
+function perkalian($data1, $data2, $data3, $data4, $data5)
+{
+if ($data1 <> $data2) {
+    // mencari potongan data pertama
+    $findcut = array_search($data3,$data4);
+    //echo array_search($data['2']['0'+'1'],$data['1']);
+    //print_r(array_slice($data['1'], 0 , $findcut)); 
+    $child01 = array_slice($data4, 0 , $findcut);
+    //print_r(array_slice($data['2'], 1 , count($data['2'])));
+    $child02 = array_slice($data5, 1 , count($data5));
+    $stillsame = array_merge($child01,$child02);
+    // Anak Pertama
+    //echo "<pre>";
+    $result['1'] = array_unique($stillsame);
+    //echo "</pre>";
+    $findcut1 = array_search($data4[$findcut],$data5);
+    $child11 = array_slice($data5, 0 , $findcut1);
+    $child12 = array_slice($data4, $findcut+1, count($data4));
+    $stillsame1 = array_merge($child11,$child12);
+    //echo "<pre>";
+    $result['2'] = array_unique($stillsame1);
+    //echo "</pre>";
+    return $result;
+}
+}
+//pemanggilan fungsi
+echo "<pre>";
+print_r(perkalian($data['1']['0'],$data['2']['0'],$data['2']['0'+'1'],$data['1'],$data['2']));
+echo "</pre>";
+
 ?>
